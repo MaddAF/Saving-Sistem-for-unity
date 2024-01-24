8bdd51aa86e7a4cb0fd4e3b16b32ef9d5b639ae6
MaddAF <129781089+MaddAF@users.noreply.github.com>
2024-01-24 12:00:18 -0300

    Update NpcClass.cs

--- NpcClass.cs
+++ NpcClass.cs
@@ -5,17 +5,14 @@ using UnityEngine;
 public class NpcClass : MonoBehaviour
 {
     public NPCData npcData;
-    //public SaveSis loader = new SaveSis();
-    public int id;
+    public int id; // note that you MUST assign an id to the npc on the editor or create some script to automaticly assign the id`s.
     // Start is called before the first frame update
     void Start()
     {
-        //npcData = loader.GetIndividuo(id);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
