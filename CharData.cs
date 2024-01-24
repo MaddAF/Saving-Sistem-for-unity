2999f18692daabbd65498633772890000d9d6fef
MaddAF <129781089+MaddAF@users.noreply.github.com>
2024-01-24 11:58:00 -0300

    Update CharData.cs

--- CharData.cs
+++ CharData.cs
@@ -1,8 +1,5 @@
 //Object of this class will hold the data
 //And then this object will be converted to JSON
-using static DialogueTrigger;
-using static DialogueManager;
-using static Npc;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,44 +8,50 @@ using System.IO.Pipes;
 
 [System.Serializable]
 
-public class NPCData {
+public class NPCData { //Add the npc parameters you want to save
+    // Don`t remove these-----
     public int Id;
     public GameObject objeto;
-    public string Name;
-    public DialogueTrigger dialog;
-    public DialogueManager dialogueManager;
     public Vector3 NpcPos;
-    public Sprite NpcSprite;
-    public Npc dialogueBallon;
     public string currentSceneNPC;
+    //----------------------------
+    //Add more parameters here:
+    
+    //exemple:
+    // public int Health;
 }
 
 public class PlayerData {
+    // Don`t remove these-----
     public GameObject playerObject;
     public Vector3 PlayerPos;
-    public Sprite PlayerSprite;
     public string currentScene;
     private GameObject x;
-    private Sprite y;
-    private void Awake()
-    {
-        x = GameObject.Find("Pocket Annie");
-        y = Resources.Load<Sprite>("Assets/Scripts/Dialogo/Sprites Dialogues/pocket annie v2-Front.png");
+    private void Awake(){
+        x = GameObject.Find("NAME OF YOUR PLAYER GAME OBJECT HERE"); //change this
     }
     public PlayerData() { 
         this.playerObject = x;
         this.PlayerPos = new Vector3((float)1.5, (float)-20.83);
-        this.PlayerSprite = y;
-        this.currentScene = "NavioMain";
+        this.currentScene = "NAME OF THE STARTING SCENE HERE";// note that the scene must be in build for this to work
     }
+    //----------------------------
+        //----------------------------
+    //Add more parameters here:
+    
+    //exemple:
+    // public int Ammo;
 }
 
 [System.Serializable] public class GameData{
-    public int desafios_completados;
-    public bool isAnnie;
+    //Add the world parameters you want to save here:
+    
+    //exemple:
+    // public int completed_achievements;
 }
 
 [System.Serializable] public class Data {
+    // If you want to add extra classes you want to save separately, create the classes like the others and add them here:
     public List<NPCData> npcs;
     public PlayerData jogador;
     public GameData gameData;
